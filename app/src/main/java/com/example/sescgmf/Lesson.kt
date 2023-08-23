e29b9b30ff99eb41d904e1eeb87e998e6b147014
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2023-08-23 20:08:01 -0300

    Update 1.0
    Update 1.0
    
    - Adicionado o layout Lesson
    - Melhorado os Layout Profile, Home,  Lesson
    - Correções de bugs

--- app/src/main/java/com/example/sescgmf/Lesson.kt
+++ app/src/main/java/com/example/sescgmf/Lesson.kt
@@ -1,8 +1,11 @@
 package com.example.sescgmf
 
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class Lesson : Fragment(R.layout.fragment_lesson)
 {
+    fun previousMonthAction(view: View) {}
+    fun nextMonthAction(view: View) {}
 
 }
